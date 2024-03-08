7dbf71748bb3fd1607591fc60447f1cda2f1862a
Vozis <78299145+Vozis@users.noreply.github.com>
2024-03-08 15:46:47 +0300

    change time

--- components/lists/schedule-list/constants.ts
+++ components/lists/schedule-list/constants.ts
@@ -5,20 +5,20 @@ export const scheduleList: IScheduleItem[] = [
     title: 'Сбор гостей',
     description:
       'Время пролетит незаметно за игристым и общением с другими гостями',
-    time: '15:30',
+    time: '16:00',
     image: '/img/schedule/schedule-1.jpg',
   },
   {
     title: 'Церемония',
     description:
       'На всякий случай приготовьте носовые платочки для трогательного момента',
-    time: '16:00',
+    time: '16:30',
     image: '/img/schedule/schedule-2.jpg',
   },
   {
     title: 'Свадебный банкет',
     description: 'Будет много танцев, веселья, поздравлений и, конечно, любви',
-    time: '16:30',
+    time: '17:00',
     image: '/img/schedule/schedule-3.jpg',
   },
   {
